e349fb2911dc87d65ca4fccea3060153762611ec
test_python <khazaei.amir110@gmail.com>
2023-08-07 18:28:23 +0330

    familiarity with if

--- js/script.js
+++ js/script.js
@@ -2,15 +2,8 @@ let num1 = 1;
 let num2 = 2;
 let num3 = 3;
 
-
-alert(num1 == num2)
-
-alert(num3 == num3)
-
-alert(num1 + num2 == num3)
-
-alert(num3 > num1)
-
-///////////////////
-
-alert ('3' === num3)
+if (num1 == num2){
+    alert('بله صحیح است')
+}else {
+    alert('خیر صحیح نیست')
+}
