7d495dc15d503766535508d28efbbc42a07d6831
test_python <khazaei.amir110@gmail.com>
2023-08-07 10:11:23 +0330

    familiarity with logical operators

--- js/script.js
+++ js/script.js
@@ -1,2 +1,24 @@
-let username = Number(prompt("password:"));
-alert(typeof username);
+let firstNumber = 12;
+let secondNumber = 20;
+let threeNumber = 2;
+let forNumber = 10;
+
+// جمع
+let plus = firstNumber + secondNumber;
+
+//تفریق
+let minus = secondNumber - forNumber;
+
+//تقسیم
+let tagsim = secondNumber / forNumber;
+
+//ضرب
+let zarb = threeNumber * forNumber;
+
+//توان
+let tavan = threeNumber **2;
+
+// باقیمانده
+let bagi = 19 % 5;
+
+alert(tavan)
