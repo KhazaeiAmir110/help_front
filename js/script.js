3920319220627eb950c0ee7e218fe5524d70651b
test_python <khazaei.amir110@gmail.com>
2023-08-07 11:32:23 +0330

    familiarity with contact

--- js/script.js
+++ js/script.js
@@ -1,13 +1,6 @@
-// let number_1 = Number(prompt("numbder one :"));
-// let number_2 = Number(prompt("number two : "));
-//
-// // alert(number_1 + number_2)
+// +
 
-// دریافت نام کاربری و رمزعبور به صورت همزمان با یک پنجره ورودی
-const userInput = prompt("لطفاً نام کاربری و رمزعبور خود را با کاما (,) جدا کنید:");
+let siteName = 'samah.ir';
+let coursName = 'JavaScript';
 
-// جدا سازی نام کاربری و رمزعبور با استفاده از کاما (,) به عنوان جداکننده
-const [username, password] = userInput.split(",");
-
-// نمایش اطلاعات وارد شده
-alert(`نام کاربری: ${username.trim()}\nرمزعبور: ${password.trim()}`);
+alert(siteName + '\n'+  coursName +'\n'+ 'salam')
