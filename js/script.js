831ad5911adb1047fa88910c8f34cfe04ae1073a
test_python <khazaei.amir110@gmail.com>
2023-08-07 18:44:39 +0330

    familiarity with AND

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ let num1 = 1;
 let num2 = 2;
 let num3 = 3;
 
-if (num1 + num2 == num2 || num3 + num3 == 5){
+if (num1 + num2 == num3 && num3 + num3 == 6){
     alert('True')
 }else {
     alert('False')
