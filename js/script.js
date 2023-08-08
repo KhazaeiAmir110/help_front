3a19e80f23c3f242530a54abbd8636ab672ca017
test_python <khazaei.amir110@gmail.com>
2023-08-08 19:37:42 +0330

    Solve the exercise Login

--- js/script.js
+++ js/script.js
@@ -1,23 +1,11 @@
-// String
-let str = " I love JS"
+function Login(username) {
+    let u = username.toLowerCase();
+    if(u === "ali"){
+        alert("TRUE :)")
+    }else {
+        alert("شما نیمتوانید وارد شوید!!! :(")
+    }
+}
+let user = prompt('Please Enter Username : ')
 
-//نمایش در console
-console.log(str)
-
-// اضافه کردن به متن اصلی
-alert(str.concat('& React'))
-
-// حذف فضای خالی در ابتدا و انتها
-alert(str.trim())
-
-// کوچک کردن همه کاراکتر ها
-alert(str.toLowerCase())
-
-// بزرگ کردن تمام کاراکترها
-alert(str.toUpperCase())
-
-// سرچ شماره ایندکس حرف
-alert(str.search("I"))
-
-// بررسی کلمه که در متن هست یا خیر؟
-alert(str.includes("love"))
+Login(user)
