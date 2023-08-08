918ed3c7c7a0574e0cafe9d3edec6578b7980755
test_python <khazaei.amir110@gmail.com>
2023-08-08 16:24:24 +0330

    familiarity with switch , case

--- js/script.js
+++ js/script.js
@@ -1,4 +1,15 @@
 let add1 = 4;
 let add2 = 3;
 
-add1 > add2 ? alert("Yes") : alert("False");
+let add3 = add1 * add2;
+
+switch (add3) {
+    case 10:
+        alert('add = 10')
+        break
+    case 12:
+        alert('add = 12')
+        break
+    default :
+        alert('add != 10 , 11')
+}
