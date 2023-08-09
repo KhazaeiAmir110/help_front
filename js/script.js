ae17cf47c15b4fed0599c5e859b1f18d9addc782
test_python <khazaei.amir110@gmail.com>
2023-08-09 09:07:34 +0330

    familiarity with String2

--- js/script.js
+++ js/script.js
@@ -1,11 +1,9 @@
-function Login(username) {
-    let u = username.toLowerCase();
-    if(u === "ali"){
-        alert("TRUE :)")
-    }else {
-        alert("شما نیمتوانید وارد شوید!!! :(")
-    }
-}
-let user = prompt('Please Enter Username : ')
+// String
 
-Login(user)
+let text = 'I love Js & Python';
+
+// برش متن با شماره ایندکس
+alert(text.slice(7,9))
+
+// برش با مبدا و تعداد رفتن به جلو
+alert(text.substr(7,2))
