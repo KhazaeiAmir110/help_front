c77e689f154464242489f894a07fe4416282ff83
test_python <khazaei.amir110@gmail.com>
2023-11-29 07:52:39 +0330

    chnage hidden css

--- miniproject/rock stone scissors/script.js
+++ miniproject/rock stone scissors/script.js
@@ -1,13 +1,19 @@
 let images = document.querySelectorAll('.img-items')
 
-for (let i = 0; i <= images.length+1; i++) {
-    images[i].addEventListener('click', () =>{heddenselect(i)})
+for (let i = 0; i <= images.length-1; i++) {
+    images[i].addEventListener("click", ()=>{hiddenselect(i),nothidden()})
 }
 
-function heddenselect(i){
+function hiddenselect(i){
     for (let j = 0; j <= images.length-1; j++) {
-        if (j =! i){
-            images[j].classList.add('hedden')
+        if (j !== i){
+            images[j].classList.add('hidden')
         }
     }
+}
+
+function nothidden(){
+    rand = Math.floor((Math.random()*10)/4)
+    cpuimages = document.querySelectorAll('.img-cpu-choosed')
+    cpuimages[rand].classList.remove('hidden')
 }
