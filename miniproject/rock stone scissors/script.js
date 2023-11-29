fc73cc309d6ae8403d2ae8fdb99be499bd32b4e2
test_python <khazaei.amir110@gmail.com>
2023-11-29 08:57:16 +0330

    Game logic

--- miniproject/rock stone scissors/script.js
+++ miniproject/rock stone scissors/script.js
@@ -1,7 +1,12 @@
 let images = document.querySelectorAll('.img-items')
+let rand
 
 for (let i = 0; i <= images.length-1; i++) {
-    images[i].addEventListener("click", ()=>{hiddenselect(i),nothidden()})
+    images[i].addEventListener("click", ()=>{
+    hiddenselect(i);
+    nothidden();
+    gamelogic(i,rand);
+    })
 }
 
 function hiddenselect(i){
@@ -16,4 +21,35 @@ function nothidden(){
     rand = Math.floor((Math.random()*10)/4)
     cpuimages = document.querySelectorAll('.img-cpu-choosed')
     cpuimages[rand].classList.remove('hidden')
+}
+
+
+function gamelogic(user,cpu){
+    const userpoint = document.querySelector('#user-point')
+    const cpupoint = document.querySelector('#cpu-point')
+    if (user !== cpu){
+        if (user==0){
+            if (cpu==1){
+                userpoint.innerHTML = Number(userpoint.innerHTML) + 1
+            }else{
+                cpupoint.innerHTML = Number(cpupoint.innerHTML) + 1
+            }
+        }
+        else if (user==1){
+            if (cpu==0){
+                cpupoint.innerHTML = Number(cpupoint.innerHTML) + 1
+            }else{
+                userpoint.innerHTML = Number(userpoint.innerHTML) + 1
+            }
+        }
+        else{
+            if (cpu==0){
+                userpoint.innerHTML = Number(userpoint.innerHTML) + 1
+            }else{
+                cpupoint.innerHTML = Number(cpupoint.innerHTML) + 1
+            }
+        }
+    }else{
+        alert("not win")
+    }
 }
