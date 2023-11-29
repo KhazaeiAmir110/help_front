dbd3d37c7909245deeec6ef1b3b9ef7f73c52327
test_python <khazaei.amir110@gmail.com>
2023-11-29 09:42:44 +0330

    Complete the project

--- miniproject/rock stone scissors/script.js
+++ miniproject/rock stone scissors/script.js
@@ -1,8 +1,10 @@
 let images = document.querySelectorAll('.img-items')
-let rand
+let cpuimages = document.querySelectorAll('.img-cpu-choosed')
+
 
 for (let i = 0; i <= images.length-1; i++) {
     images[i].addEventListener("click", ()=>{
+    rand = Math.floor((Math.random()*10)/4)
     hiddenselect(i);
     nothidden();
     gamelogic(i,rand);
@@ -10,6 +12,7 @@ for (let i = 0; i <= images.length-1; i++) {
 }
 
 function hiddenselect(i){
+    images = document.querySelectorAll('.img-items')
     for (let j = 0; j <= images.length-1; j++) {
         if (j !== i){
             images[j].classList.add('hidden')
@@ -18,8 +21,6 @@ function hiddenselect(i){
 }
 
 function nothidden(){
-    rand = Math.floor((Math.random()*10)/4)
-    cpuimages = document.querySelectorAll('.img-cpu-choosed')
     cpuimages[rand].classList.remove('hidden')
 }
 
@@ -28,28 +29,40 @@ function gamelogic(user,cpu){
     const userpoint = document.querySelector('#user-point')
     const cpupoint = document.querySelector('#cpu-point')
     if (user !== cpu){
-        if (user==0){
-            if (cpu==1){
+        if (user===0){
+            if (cpu===1){
                 userpoint.innerHTML = Number(userpoint.innerHTML) + 1
+                document.getElementById('result_box').style.backgroundColor = '#7df275'
             }else{
                 cpupoint.innerHTML = Number(cpupoint.innerHTML) + 1
+                document.getElementById('result_box').style.backgroundColor = '#f44747'
             }
         }
-        else if (user==1){
-            if (cpu==0){
+        else if (user===1){
+            if (cpu===0){
                 cpupoint.innerHTML = Number(cpupoint.innerHTML) + 1
             }else{
                 userpoint.innerHTML = Number(userpoint.innerHTML) + 1
+                document.getElementById('result_box').style.backgroundColor = '#7df275'
             }
         }
         else{
-            if (cpu==0){
+            if (cpu===0){
                 userpoint.innerHTML = Number(userpoint.innerHTML) + 1
+                document.getElementById('result_box').style.backgroundColor = '#7df275'
             }else{
                 cpupoint.innerHTML = Number(cpupoint.innerHTML) + 1
             }
         }
     }else{
         alert("not win")
+    }
+}
+
+
+function refresh(){
+    for(let i=0; i<= images.length - 1 ; i++){
+        images[i].classList.remove("hidden")
+        cpuimages[i].classList.add("hidden")
     }
 }
