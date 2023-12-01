c3aeefba88031c1ef2a4c8e9881c59cc300be753
test_python <khazaei.amir110@gmail.com>
2023-12-01 15:33:12 +0330

    call and apply

--- js/script.js
+++ js/script.js
@@ -1,10 +1,15 @@
-let firsname = 'Ali'
-// console.log(this)
-
 const user = {
-    firstname : 'John',
-    lastname : 'Doe',
-    test : () => {
-        console.log(this.firsname)
+    firstname : 'Ahmad',
+    lastname : 'Ahmadi',
+    age : 36,
+}
+
+const user1 = {
+    test : function red(city) {
+        alert(this.firstname + city)
     }
 }
+
+// Difference call and apply
+user1.test.call(user, 'Qom')
+user1.test.apply(user, ['qom'])
