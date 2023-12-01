2415511ffb034ba46d6f75735b3ea24897709a09
test_python <khazaei.amir110@gmail.com>
2023-12-01 15:05:48 +0330

    start work with this

--- js/script.js
+++ js/script.js
@@ -1,15 +1,10 @@
-const obj1 = {
-    age:14,
-    test:() => 23 + 23
+let firsname = 'Ali'
+// console.log(this)
+
+const user = {
+    firstname : 'John',
+    lastname : 'Doe',
+    test : () => {
+        console.log(this.firsname)
+    }
 }
-
-alert(obj1.age)
-alert(obj1.test())
-
-
-let array1 = [34,23,23,13,63,63]
-let array2 = new Array()
-
-alert(array1)
-array2.push(34)
-alert(array2)
