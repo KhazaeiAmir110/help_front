422e2bf0cb1270ab4127ae8f2957d57b5198cee5
test_python <khazaei.amir110@gmail.com>
2023-12-08 21:25:29 +0330

    use strict

--- js/script.js
+++ js/script.js
@@ -1,9 +1,4 @@
-const math = Math
-console.log(math.PI) // 3.14
-console.log(math.E) // 2.7
-console.log(math.log10(10)) // 1
-console.log(math.log2(256)) // 8
-console.log(math.sinh(30*math.PI/180)) // 0.5
-console.log(math.cosh(30*math.PI/180)) // 1
-console.log(math.floor(math.random()*10)) //random code 1 - 10
+'use strict'
 
+x = 12 // error message
+console.log(x)
