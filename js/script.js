2c0a690c46ae828be0f0cfa7e8aaf2335b604a69
test_python <khazaei.amir110@gmail.com>
2023-12-09 16:57:51 +0330

    redusee

--- js/script.js
+++ js/script.js
@@ -1,4 +1,9 @@
-'use strict'
+// redusee
 
-x = 12 // error message
-console.log(x)
+myarray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+
+const reducer = (accumulator, currentValue) => accumulator + currentValue
+
+const ressult = myarray.reduce(reducer)
+
+console.log(ressult)
