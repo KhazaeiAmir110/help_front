58788f4735bfc21ef8e6546f0e042bd832699e0c
test_python <khazaei.amir110@gmail.com>
2023-12-09 17:13:36 +0330

    start work with Object Oriented

--- js/script.js
+++ js/script.js
@@ -1,9 +1,20 @@
-// redusee
+// Object Oriented
 
-myarray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+class Users {
+    constructor(name, age, password) {
+        this.name = name;
+        this.age = age;
+        this.password = password;
+    }
+    showAge(){
+        let data = new Date();
+        let year = data.getFullYear();
+        let result = year - this.age;
+        console.log(result);
+    }
+}
 
-const reducer = (accumulator, currentValue) => accumulator + currentValue
+let user_1 = new Users('amir',21,123)
 
-const ressult = myarray.reduce(reducer)
-
-console.log(ressult)
+console.log(user_1.age)
+user_1.showAge()
