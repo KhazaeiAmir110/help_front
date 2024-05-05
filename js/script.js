1c6364ccca135cc6a17e45b43d8f2aa3f95f85d3
test_python <khazaei.amir110@gmail.com>
2024-05-05 20:20:43 +0330

    fix js

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+// Page 1
+
 function displayText() {
       var selectElement = document.getElementById('mySelect');
       var selectedValue = selectElement.value;
@@ -5,15 +7,18 @@ function displayText() {
       var value_1 = document.getElementById('value_1');
       var value_2 = document.getElementById('value_2');
 
+      var elements = [];
 
-      if (selectedValue === "0"){
-        document.querySelector('#value_1').style.display = 'none';
-        document.querySelector('#value_2').style.display = 'none';
-      } else if (selectedValue === "1") {
-        document.querySelector('#value_1').style.display = 'list-item';
-        document.querySelector('#value_2').style.display = 'none';
-      } else if (selectedValue === "2") {
-          document.querySelector('#value_1').style.display = 'none';
-          document.querySelector('#value_2').style.display = 'list-item';
+      elements.push(value_1);
+      elements.push(value_2);
+
+      for (var i = 0; i < elements.length; i++) {
+            elements[i].style.display = 'none';
+      }
+
+
+      if (selectedValue >= 1 && selectedValue <= elements.length){
+            elements[selectedValue - 1].style.display = 'list-item';
       }
 }
+
