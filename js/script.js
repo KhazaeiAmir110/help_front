589fce85b8d1bda308038820003e560972a1408c
test_python <khazaei.amir110@gmail.com>
2024-05-05 19:59:28 +0330

    update js page 1

--- js/script.js
+++ js/script.js
@@ -1,19 +1,19 @@
 function displayText() {
-    var selectElement = document.getElementById('mySelect');
-    var selectedValue = selectElement.value;
-    var textElement = document.getElementById('textToShow');
-    var text = document.getElementById('text');
+      var selectElement = document.getElementById('mySelect');
+      var selectedValue = selectElement.value;
 
-    if (selectedValue === "0") {
-    textElement.innerHTML = "متن برای همه";
-    document.querySelector('#textToShow').style.display = 'flex';
-    } else if (selectedValue === "2") {
-    textElement.innerHTML = "متن برای احسان امیری";
-    document.querySelector('#textToShow').style.display = 'inline';
+      var value_1 = document.getElementById('value_1');
+      var value_2 = document.getElementById('value_2');
 
-    document.querySelector('#text').style.display = 'list-item';
-    
-    } else {
-    textElement.innerHTML = "متن اولیه";
-    }
+
+      if (selectedValue === "0"){
+        document.querySelector('#value_1').style.display = 'none';
+        document.querySelector('#value_2').style.display = 'none';
+      } else if (selectedValue === "1") {
+        document.querySelector('#value_1').style.display = 'list-item';
+        document.querySelector('#value_2').style.display = 'none';
+      } else if (selectedValue === "2") {
+          document.querySelector('#value_1').style.display = 'none';
+          document.querySelector('#value_2').style.display = 'list-item';
+      }
 }
