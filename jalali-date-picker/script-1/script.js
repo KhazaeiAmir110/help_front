0eed1237cc39fe97595724736b10454be01d5470
test_python <khazaei.amir110@gmail.com>
2024-05-12 11:41:34 +0330

    date picker one

---
+++ jalali-date-picker/script-1/script.js
@@ -0,0 +1,10 @@
+/*
+document date picker :
+1) https://github.com/majidh1/JalaliDatePicker?tab=readme-ov-file
+2) https://codepen.io/search/pens?q=%D8%AA%D8%A7%D8%B1%DB%8C%D8%AE+%D8%B4%D9%85%D8%B3%DB%8C
+3) https://codepen.io/majidh1/pen/qBRQBzd
+4) https://majidh1.github.io/JalaliDatePicker/
+
+*/
+
+jalaliDatepicker.startWatch();
