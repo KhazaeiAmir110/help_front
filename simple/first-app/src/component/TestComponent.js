fdcbce564db157808fd3d51c2174e7b8ade67137
test_python <khazaei.amir110@gmail.com>
2024-05-16 07:17:36 +0330

    work with class and function in react

--- simple/first-app/src/component/TestComponent.js
+++ simple/first-app/src/component/TestComponent.js
@@ -1,9 +1,52 @@
+import React, {Component} from "react";
 
+class User extends Component{
+    state = {
+        one : {
+            name  : 'Amir',
+            age : 22
+        },
+        two : {
+            name  : 'Ali',
+            age : 21
+        },
+        three : {
+            name  : 'Javid',
+            age : 32
+        },
+        four : {
+            name  : 'Gadget',
+            age : 42
+        },
+    }
+    render(){
+        return (
+            <>
+                <ul>
+                    <li>
+                        name : {this.state.one.name}
+                    </li>
+                    <li>
+                        name : {this.state.two.name}
+                    </li>
+                    <li>
+                        name : {this.state.three.name}
+                    </li>
+                    <li>
+                        name : {this.state.four.name}
+                    </li>
+                </ul>
+            </>
+        )
+    }
+
+}
 
 export default function Test() {
     return (
         <>
             <h1>Hellow file Component</h1>
+            <User />
         </>
     )
 }
--- simple/first-app/src/index.js
+++ simple/first-app/src/index.js
@@ -1,51 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client"
 import Test from "./component/TestComponent";
-
-const name = "Amir"
-const element = (
-    <div>
-        <h1>
-            first one
-        </h1>
-        <h2 className="test">
-            Hellow {name}
-        </h2>
-    </div>
-);
-
-class TestClass extends React.Component{
-    render() {
-        return (
-            <>
-                <h1>Hellow Component Class</h1>
-                {element}
-                <Test />
-            </>
-        )
-    }
-}
-
-function TestFunction() {
-    return <h1>Hellow Component Function</h1>
-}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  // <React.StrictMode>
-  //   <App />
-  // </React.StrictMode>
-  //   element,
     <>
-        <TestClass />
-        <TestFunction />
+        <Test />
     </>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
