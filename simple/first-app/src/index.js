367a59f7c6d788780d067cc1751e4f4588777ed2
test_python <khazaei.amir110@gmail.com>
2024-05-16 05:32:55 +0330

    work with JSX

--- simple/first-app/src/index.js
+++ simple/first-app/src/index.js
@@ -4,11 +4,24 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+const name = "Amir"
+const element = (
+    <div>
+        <h1>
+            first one
+        </h1>
+        <h2 className="test">
+            Hellow {name}
+        </h2>
+    </div>
+);
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  // <React.StrictMode>
+  //   <App />
+  // </React.StrictMode>
+    element
 );
 
 // If you want to start measuring performance in your app, pass a function
