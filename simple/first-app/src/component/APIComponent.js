1a4785fcf869c78023b64efacf75ed19c148b37c
test_python <khazaei.amir110@gmail.com>
2024-05-17 15:15:30 +0330

    findDomNode

--- simple/first-app/src/component/APIComponent.js
+++ simple/first-app/src/component/APIComponent.js
@@ -1,23 +1,32 @@
 import React, {Component} from "react";
+import ReactDOM from "react-dom";
 
 class Api_Test extends Component{
-    constructor(){
-        super();
-        this.state = {
-            date : ['Hellow ', 'Hellow']
-        }
-        // this.setState = this.setState.bind(this);
-    }
 
+
+    // forceUpdate
     setStateHandler = () => {
         this.forceUpdate();
+    }
+
+    // find dom node
+    findDomNodeHandler = () =>{
+        let eleman = document.getElementById('itme1')
+        ReactDOM.findDOMNode(eleman).style.color = 'red'
     }
 
     render(){
         return (
             <>
+                {/* forceUpdate */}
                 <button onClick={this.setStateHandler}>Update</button>
                 <p>{Math.random()}</p>
+
+
+                {/* find dom node */}
+                <h3>Find Dom Node</h3>
+                <button onClick={this.findDomNodeHandler}>Update Color</button>
+                <div id="item1">This is a Item One</div>
             </>
         )
     }
