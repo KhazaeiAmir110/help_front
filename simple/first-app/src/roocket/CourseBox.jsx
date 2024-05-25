9f6fcd58b235ec75b95ad350fa593998d3895f00
test_python <khazaei.amir110@gmail.com>
2024-05-25 18:45:39 +0330

    work with useState

--- simple/first-app/src/roocket/CourseBox.jsx
+++ simple/first-app/src/roocket/CourseBox.jsx
@@ -1,11 +1,36 @@
-
+import {useState} from "react";
 
 export default function CourseBox (props) {
+
+    let [clickCount, setClickCount] = useState(0)
+
+    const clickHandler = () => {
+        setClickCount(clickCount + 1)
+        if (clickCount >= 1000){
+            setClickCount(clickCount=0)
+        }
+    }
+
+    const DoubleClick = () => {
+        setClickCount(clickCount + 100)
+    }
+
     return (
-        <div>
-            <h1>{props?.title}</h1>
-            <p>{props?.description}</p>
-            <span>time : {props?.time}</span>
-        </div>
+        <>
+            {/*props*/}
+            <div>
+                <h1>{props?.title}</h1>
+                <p>{props?.description}</p>
+                <span>time : {props?.time}</span>
+            </div>
+
+            <hr /><br />
+            {/*  useState  */}
+            <div>
+                <h1>useState</h1>
+                <button onClick={clickHandler} onDoubleClick={DoubleClick}>Click Me !</button>
+                <p>{clickCount}</p>
+            </div>
+        </>
     )
 }
