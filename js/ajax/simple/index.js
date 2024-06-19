0938b4939c4abe36299794a7ab3fdd48f5ba144e
test_python <khazaei.amir110@gmail.com>
2024-06-19 18:52:29 +0330

    work with ajax

--- js/ajax/simple/index.js
+++ js/ajax/simple/index.js
@@ -7,7 +7,19 @@ function loadDoc() {
         document.getElementById('demo').innerHTML = this.responseText;
     }
 
-    xhr.open("GET", 'info.txt', true)
+    xhr.open("GET", 'help.html', true)
 
+    xhr.send()
+}
+
+function send() {
+    var xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function () {
+        var val = document.getElementById('input_id').value;
+        if (val === 'amir'){
+            document.getElementById('titr').innerHTML = "Hellow Amir"
+        }
+    }
+    xhr.open("POST")
     xhr.send()
 }
