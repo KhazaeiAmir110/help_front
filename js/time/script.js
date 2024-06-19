d66a83766ea53a2a969ff7ffe8f751f570e50524
test_python <khazaei.amir110@gmail.com>
2024-06-19 19:24:38 +0330

    add excludeStart, excludeEnd in function

--- js/time/script.js
+++ js/time/script.js
@@ -22,12 +22,12 @@ function generateTimeSlots(startTime, endTime, duration) {
 }
 
 // مثال استفاده
-let startTime = "10:00";
-let endTime = "18:00";
-let duration = 30; // مدت زمان به دقیقه
-
-let timeSlots = generateTimeSlots(startTime, endTime, duration);
-console.log(timeSlots);
+// let startTime = "10:00";
+// let endTime = "18:00";
+// let duration = 30; // مدت زمان به دقیقه
+//
+// let timeSlots = generateTimeSlots(startTime, endTime, duration);
+// console.log(timeSlots);
 
 
 // ------------------------------------------------------------------
@@ -58,3 +58,36 @@ let durationtime = 30;
 let timeslots = getTime(starttime, endtime, durationtime);
 console.log(timeslots);
 
+// ----------------------------------------------------------------------------
+
+function GetTime(startTime, endTime, duration, excludeStart, excludeEnd) {
+    let start = new Date(`1970-01-01T${startTime}`);
+    let end = new Date(`1970-01-01T${endTime}`);
+    let excludeStartTime = new Date(`1970-01-01T${excludeStart}`);
+    let excludeEndTime = new Date(`1970-01-01T${excludeEnd}`);
+    let result = [];
+
+    let durationInMs = duration *60 *  1000;
+
+    while (start <= end) {
+        if (start < excludeStartTime || start > excludeEndTime) {
+            let hours = start.getHours().toString().padStart(2, '0');
+            let minutes = start.getMinutes().toString().padStart(2, '0');
+            let seconds = start.getSeconds().toString().padStart(2, '0');
+            result.push(`${hours}:${minutes}:${seconds}`);
+        }
+
+        start = new Date(start.getTime() + durationInMs);
+    }
+
+    return result;
+}
+
+let startTime = "10:00:00";
+let endTime = "18:00:00";
+let duration = 30; // مدت زمان به ثانیه (30 دقیقه)
+let excludeStart = "12:00:00";
+let excludeEnd = "13:30:00";
+
+let timeSlots = GetTime(startTime, endTime, duration, excludeStart, excludeEnd);
+console.log(timeSlots);
