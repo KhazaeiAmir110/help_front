6fdb564c333c920cd0ec288ce9669ecdafa1a9e3
KhazaeiAmir110 <khazaei.amir110@gmail.com>
2024-07-12 08:32:41 +0330

    usage list on useState

--- simple/second-app/src/App.js
+++ simple/second-app/src/App.js
@@ -1,10 +1,18 @@
 import './App.css';
 import {useState} from "react";
+import * as events from "events";
 
 function App() {
     // let name = "Mahdi"
     const [name, setName] = useState('Mahdi')
 
+    // list on useState
+    const [events , setEvents] = useState([
+    {title: 'In the name of god', id: 1},
+    {title: 'hellow word', id: 2},
+    {title: 'i am amir hossein', id: 3},
+    {title: 'In the name of god', id: 4}
+    ])
     const click = () => {
         // name = "Amir"
         setName('amir')
@@ -14,6 +22,17 @@ function App() {
     <div className="App">
         <h1>{name}</h1>
       <button onClick={click}>Click me</button>
+
+        <hr />
+        {/*// usage list*/}
+        {
+        events.map((event, index) => (
+            <div style={{display:"flex", justifyContent:"center", alignItems:"center"}} key={event.id}>
+                <h3 style={{color:'blue'}}>{index}_ </h3> <h3>{event.title}</h3>
+            </div>
+        ))
+        }
+        <hr />
     </div>
   );
 }
