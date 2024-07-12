c289f43c921fa54a203651f9f9aa8fdc8c8e1693
KhazaeiAmir110 <khazaei.amir110@gmail.com>
2024-07-12 07:56:59 +0330

    usage useState

--- simple/second-app/src/App.js
+++ simple/second-app/src/App.js
@@ -1,9 +1,19 @@
 import './App.css';
+import {useState} from "react";
 
 function App() {
+    // let name = "Mahdi"
+    const [name, setName] = useState('Mahdi')
+
+    const click = () => {
+        // name = "Amir"
+        setName('amir')
+    }
+
   return (
     <div className="App">
-
+        <h1>{name}</h1>
+      <button onClick={click}>Click me</button>
     </div>
   );
 }
