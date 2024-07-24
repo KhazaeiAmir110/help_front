02085f1690a95c6e85d019d0601f9c2161ab512e
KhazaeiAmir110 <khazaei.amir110@gmail.com>
2024-07-24 12:07:43 +0330

    fix project calculator

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -1,8 +1,11 @@
 import './App.css';
 
+// eslint-disable-next-line no-unused-vars
+import Mashing from "./calculator/mashin";
+
 function App() {
     return (
-        <div></div>
+        <Mashing />
     );
 }
 
