b3633bd9c1d7840706d92b43cfb9e79d183e5573
KhazaeiAmir110 <khazaei.amir110@gmail.com>
2024-07-31 17:14:41 +0330

    React.Fragment

--- reactapp/src/components/first/first.js
+++ reactapp/src/components/first/first.js
@@ -1,10 +1,14 @@
 import './first.css'
+import React from "react";
+
 export default function First(props) {
-    return(
-        <div className='base'>
-            <h1 className="title">{props.title}</h1>
-            <br/>
-            <h3 className="subtitle">components this is a best</h3>
-        </div>
+    return (
+        <React.Fragment>
+            <div className='base'>
+                <h1 className="title">{props.title}</h1>
+                <br/>
+                <h3 className="subtitle">components this is a best</h3>
+            </div>
+        </React.Fragment>
     )
 }
