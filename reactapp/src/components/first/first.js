9fe18f628a1b485721185b5ff4c630851f886ec8
KhazaeiAmir110 <khazaei.amir110@gmail.com>
2024-07-31 16:54:01 +0330

    add style in component first

--- reactapp/src/components/first/first.js
+++ reactapp/src/components/first/first.js
@@ -1,9 +1,10 @@
-
+import './first.css'
 export default function First() {
     return(
-        <div>
-            <h1>Start Work With Components</h1>
-            <p>components this is a best</p>
+        <div className='base'>
+            <h1 className="title">Start Work With Components</h1>
+            <br/>
+            <h3 className="subtitle">components this is a best</h3>
         </div>
     )
 }
