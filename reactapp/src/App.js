fee03c7855abbb30662d9e1028c6c4030098d6eb
KhazaeiAmir110 <khazaei.amir110@gmail.com>
2024-07-31 17:32:31 +0330

    send props template

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import First from "./components/first/first";
+import Modal from "./components/modal/Modal";
 
 export default function App() {
     return (
@@ -9,6 +10,13 @@ export default function App() {
             <br/><hr/>
 
             <First title="End Props"/>
+
+            <br/><hr/>
+
+            <Modal>
+                <h2>10% off Coupon Code</h2>
+                <p>Use the Code OPEN-CODE</p>
+            </Modal>
         </div>
     );
 }
