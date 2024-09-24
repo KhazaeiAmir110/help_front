047bfbf0c51bd01aaa646b81fb8dbafc29b069ee
KhazaeiAmir110 <khazaei.amir110@gmail.com>
2024-09-24 17:16:09 +0330

    work with navbar

--- src/components/header/Navigation.jsx
+++ src/components/header/Navigation.jsx
@@ -2,10 +2,22 @@ import React from 'react';
 
 function Navigation(props) {
     return (
-        <nav className={'container'}>
-            <p>Logo</p>
-            <p>Meno item</p>
-            <p>test</p>
+        <nav className={'container flex mt-10 items-center justify-center text-slate-300'}>
+            <div className={'flex items-center'}>
+                <h1 className={'text-3xl mr-12'}>HyperMovies</h1>
+                <ul className={'flex gap-4 text uppercase'}>
+                    <li><a href={'#'}>Movies</a></li>
+                    <li><a href={'#'}>TV shows</a></li>
+                    <li><a href={'#'}>People</a></li>
+                    <li><a href={'#'}>More</a></li>
+                </ul>
+            </div>
+            <div className={'ml-auto text uppercase'}>
+                <ul className={'flex gap-4'}>
+                    <li><a className={''} href={'#'}>Login</a></li>
+                    <li><a className={'bg-rose-700 px-6 py-3 rounded-2xl'} href={'#'}>Sign up</a></li>
+                </ul>
+            </div>
         </nav>
     );
 }
