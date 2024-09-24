a16f6a90700fc763c23d359cadc5df29818b9fd5
KhazaeiAmir110 <khazaei.amir110@gmail.com>
2024-09-24 20:50:58 +0330

    add search in project

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import Header from "./components/header/Header.jsx";
+import SearchBox from "./components/header/SearchBox.jsx";
 
 function App() {
     return (
         <>
-            <Header></Header>
+            <Header/>
+            <SearchBox/>
         </>
     )
 }
