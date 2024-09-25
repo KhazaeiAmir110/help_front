b4e8cf96146e1049bab7887dfaa8a7d75a709b41
KhazaeiAmir110 <khazaei.amir110@gmail.com>
2024-09-25 19:48:44 +0330

    start work with responsive

--- src/components/header/Navigation.jsx
+++ src/components/header/Navigation.jsx
@@ -7,14 +7,14 @@ function Navigation(props) {
                 <h1 className={'text-2xl mr-12 hover:text-red-200'}>
                     Hyper<br /><span className={'text-5xl text-rose-600 hover:text-rose-900'}>Movies</span>
                 </h1>
-                <ul className={'flex gap-4 text uppercase'}>
+                <ul className={'hidden md:flex gap-4 text uppercase'}>
                     <li><a href={'#'}>Movies</a></li>
                     <li><a href={'#'}>TV shows</a></li>
                     <li><a href={'#'}>People</a></li>
                     <li><a href={'#'}>More</a></li>
                 </ul>
             </div>
-            <div className={'ml-auto text uppercase'}>
+            <div className={'hidden md:block ml-auto text uppercase'}>
                 <ul className={'flex gap-4'}>
                     <li><a className={''} href={'#'}>Login</a></li>
                     <li><a className={'bg-rose-800 hover:bg-rose-700 px-6 py-3 rounded-2xl'} href={'#'}>Sign up</a></li>
