6e87a26cb39278604873b0e279673e33f2fc584e
KhazaeiAmir110 <khazaei.amir110@gmail.com>
2024-09-26 19:18:00 +0330

    create Component MovieListSlider

---
+++ src/components/movies/MoviesListSlider.jsx
@@ -0,0 +1,43 @@
+import {Swiper, SwiperSlide} from "swiper/react";
+import {Autoplay} from "swiper/modules";
+
+function MovieListSlider() {
+    return (
+        <div className={"mt-8"}>
+            <Swiper
+                modules={[Autoplay]}
+                autoplay={true} loop={true}
+                breakpoints={{
+                    // when window width is >= 320px
+                    640: {
+                        slidesPerView: 2,
+                        spaceBetween: 20
+                    },
+                    // when window width is >= 480px
+                    768: {
+                        slidesPerView: 3,
+                        spaceBetween: 30
+                    },
+                    // when window width is >= 640px
+                    1024: {
+                        slidesPerView: 4,
+                        spaceBetween: 40
+                    }
+                }}>
+                {[1, 2, 3, 4, 1, 2, 3, 4].map(
+                        number => (
+                            <SwiperSlide key={number}>
+                                <img
+                                    className={""}
+                                    src={`http://busterhtml.mbkip3ms9u-e92498n216kr.p.temp-site.link/images/uploads/slider${number}.jpg`}
+                                    alt=""/>
+                            </SwiperSlide>
+                        )
+                    )
+                }
+            </Swiper>
+        </div>
+    );
+}
+
+export default MovieListSlider;
