16df2d45c87d6d3a6c2c687494755d0a9c1e1580
KhazaeiAmir110 <khazaei.amir110@gmail.com>
2024-09-26 22:45:09 +0330

    add footer in site

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Header from "./components/header/Header.jsx";
 import Main from "./components/main/Main.jsx";
+import Footer from "./components/footer/Footer.jsx";
 
 function App() {
     return (
         <>
             <Header />
             <Main />
+            <Footer />
         </>
     )
 }
---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,10 @@
+
+function Footer() {
+    return (
+        <footer className={"text-center bg-slate-900 py-6 mt-6"}>
+            footer
+        </footer>
+    );
+}
+
+export default Footer;
