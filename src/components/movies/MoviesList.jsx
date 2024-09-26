b17aa7a28ac6a3ca8870f3e122c09787d63b2789
KhazaeiAmir110 <khazaei.amir110@gmail.com>
2024-09-26 20:01:45 +0330

    responsive movieList

--- src/components/movies/MoviesList.jsx
+++ src/components/movies/MoviesList.jsx
@@ -4,7 +4,7 @@ import MovieCard from "./MovieCard.jsx";
 function MoviesList() {
     return (
         <div className={"container"}>
-            <div className="py-8 columns-4 mt-12">
+            <div className="columns-3 md:columns-6 mt-12">
                 <MovieCard
                     img="http://busterhtml.mbkip3ms9u-e92498n216kr.p.temp-site.link/images/uploads/mv-item1.jpg"
                     title = "Interstellar"
@@ -24,6 +24,16 @@ function MoviesList() {
                     img = "http://busterhtml.mbkip3ms9u-e92498n216kr.p.temp-site.link/images/uploads/mv-item4.jpg"
                     title = "The walk"
                     star = "9.1"
+                />
+                 <MovieCard
+                    img = "http://busterhtml.mbkip3ms9u-e92498n216kr.p.temp-site.link/images/uploads/mv-item5.jpg"
+                    title = "Intore wild"
+                    star = "8.3"
+                />
+                 <MovieCard
+                    img = "http://busterhtml.mbkip3ms9u-e92498n216kr.p.temp-site.link/images/uploads/mv-item7.jpg"
+                    title = "Gravity"
+                    star = "6.6"
                 />
             </div>
         </div>
