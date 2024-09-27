07ddbf3286ac75e8bc4cb38d3728d883b21ee146
KhazaeiAmir110 <khazaei.amir110@gmail.com>
2024-09-27 23:08:15 +0330

    get api movie or tv

--- src/components/pages/single/MovieTV.jsx
+++ src/components/pages/single/MovieTV.jsx
@@ -1,6 +1,30 @@
-function MovieTV() {
+import {useParams} from "react-router-dom";
+import {useEffect, useState} from "react";
+import axios from "axios";
+import {CONFIG} from "../../../config.js";
+
+function MovieTV(props) {
+
+    const [movieTv, setMovieTv] = useState([]);
+
+    const {id} = useParams();
+
+    async function fetchMovieTV() {
+        const {data} = await axios.get(
+            `https://api.themoviedb.org/3/${props.type}/${id}?api_key=${CONFIG.apiKey}`
+        );
+
+        setMovieTv(data)
+    }
+
+    useEffect(() => {
+        fetchMovieTV()
+    }, [id])
+
     return (
-        <div>Movie AND TV</div>
+        <div>
+            <h1>{movieTv.title}</h1>
+        </div>
     );
 }
 
