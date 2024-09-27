e1824d9fabf56d57724efeb2e778e678cfb147ac
KhazaeiAmir110 <khazaei.amir110@gmail.com>
2024-09-27 20:28:16 +0330

    update header => location.pathname

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import {useLocation} from "react-router-dom";
+
 import Navigation from "./Navigation.jsx";
 import SearchBox from "./SearchBox.jsx";
 import FollowUs from "./FollowUs.jsx";
@@ -6,16 +7,26 @@ import Slider from "./Slider.jsx";
 
 import image_slide from "/images/header/cinema.jpg"
 
-const Header = () => (
-    <div className={"w-full"}>
-        <header className={"container bg-cover py-0.5 md:pt-1 md:pb-12 items-center justify-center"}
-                style={{backgroundImage: `linear-gradient(to bottom, #00000085, #000000db),url(${image_slide})`}}>
-            <Navigation />
-            <SearchBox />
-            <FollowUs />
-            <Slider />
-        </header>
-    </div>
-);
+
+function Header() {
+    const location = useLocation();
+    return (
+        <div className={"w-full"}>
+            <header className={"container bg-cover py-0.5 md:pt-1 md:pb-12 items-center justify-center"}
+                    style={{backgroundImage: `linear-gradient(to bottom, #00000085, #000000db),url(${image_slide})`}}>
+                <Navigation/>
+                {
+                    location.pathname === "/" && (
+                        <>
+                            <SearchBox/>
+                            <FollowUs/>
+                            <Slider/>
+                        </>
+                    )
+                }
+            </header>
+        </div>
+    )
+}
 
 export default Header;
