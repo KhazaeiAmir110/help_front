5a199c7f015633e0b917e089f05c0817107af94b
KhazaeiAmir110 <khazaei.amir110@gmail.com>
2024-09-27 11:14:29 +0330

    add api in MoviesList.jsx

--- src/components/movies/MoviesList.jsx
+++ src/components/movies/MoviesList.jsx
@@ -1,66 +1,32 @@
 import MovieListSlider from "./MoviesListSlider.jsx";
-
-const moviesList = [
-    {
-        url:
-            "http://busterhtml.mbkip3ms9u-e92498n216kr.p.temp-site.link/images/uploads/mv-item1.jpg",
-        title:
-            "Interstellar",
-        star:
-            "7.4"
-    },
-    {
-        url:
-            "http://busterhtml.mbkip3ms9u-e92498n216kr.p.temp-site.link/images/uploads/mv-item2.jpg",
-        title:
-            "The reverent",
-        star:
-            "4.3"
-    },
-    {
-        url:
-            "http://busterhtml.mbkip3ms9u-e92498n216kr.p.temp-site.link/images/uploads/mv-item3.jpg",
-        title:
-            "Die hard",
-        star:
-            "9.1"
-    },
-    {
-        url:
-            "http://busterhtml.mbkip3ms9u-e92498n216kr.p.temp-site.link/images/uploads/mv-item4.jpg",
-        title:
-            "The walk",
-        star:
-            "6.4"
-    },
-    {
-        url:
-            "http://busterhtml.mbkip3ms9u-e92498n216kr.p.temp-site.link/images/uploads/mv-item5.jpg",
-        title:
-            "Intone wild",
-        star:
-            "8.3"
-    },
-    {
-        url:
-            "http://busterhtml.mbkip3ms9u-e92498n216kr.p.temp-site.link/images/uploads/mv-item7.jpg",
-        title:
-            "Gravity",
-        star:
-            "10"
-    },
-    {
-        url:
-            "http://busterhtml.mbkip3ms9u-e92498n216kr.p.temp-site.link/images/uploads/mv-item8.jpg",
-        title:
-            "Southpaw",
-        star:
-            "5.5"
-    }
-]
+import {useEffect, useState} from "react";
+import axios from "axios";
+import {CONFIG} from "../../config.js";
 
 
 function MoviesList() {
+
+    const [movies, setMovies] = useState({trending: [], onTv: []});
+
+    async function loadMovies(type, path) {
+
+        const url = `${CONFIG.baseURL}/${path}?api_key=${CONFIG.apiKey}`
+        const {data} = await axios.get(url);
+
+        setMovies(prevMovies => ({
+            ...prevMovies,
+            [type]: data.results
+        }));
+    }
+
+    useEffect(() => {
+        loadMovies("trending", "trending/movie/day");
+    }, [])
+
+    useEffect(() => {
+        loadMovies("onTv", "tv/popular")
+    }, [])
+
     return (
         <div className={"container"}>
             <div className={"mt-12 mb-12"}>
@@ -74,7 +40,7 @@ function MoviesList() {
                         <li className={"hover:text-[#dcf836]"}>This Year</li>
                     </ul>
                 </div>
-                <MovieListSlider moveis={moviesList}/>
+                <MovieListSlider moveis={movies.trending}/>
             </div>
 
             <div className={"mt-12 mb-12"}>
@@ -87,7 +53,7 @@ function MoviesList() {
                         <li className={"hover:text-[#dcf836]"}>Top rated</li>
                     </ul>
                 </div>
-                <MovieListSlider moveis={moviesList}/>
+                <MovieListSlider moveis={movies.onTv}/>
             </div>
         </div>
     );
