3886daddc1fed73e864aec2f95cfc6c7a0e1a2d1
KhazaeiAmir110 <khazaei.amir110@gmail.com>
2024-09-28 17:27:18 +0330

    update breakpoints in swiper

--- src/components/movies/MoviesListSlider.jsx
+++ src/components/movies/MoviesListSlider.jsx
@@ -15,18 +15,18 @@ function MovieListSlider(props) {
                         breakpoints={{
                             // when window width is >= 320px
                             640: {
-                                slidesPerView: 3,
-                                spaceBetween: 10
+                                slidesPerView: 4,
+                                spaceBetween: 8
                             },
                             // when window width is >= 480px
                             768: {
-                                slidesPerView: 4,
-                                spaceBetween: 20
+                                slidesPerView: 6,
+                                spaceBetween: 15
                             },
                             // when window width is >= 640px
                             1024: {
-                                slidesPerView: 6,
-                                spaceBetween: 30
+                                slidesPerView: 9,
+                                spaceBetween: 20
                             }
                         }}>
                         {props.moveis.map(
