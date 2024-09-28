6aa14847761ce0f95106cc1ef643998c2737016d
KhazaeiAmir110 <khazaei.amir110@gmail.com>
2024-09-28 17:16:29 +0330

    update page person

--- src/components/pages/single/Person.jsx
+++ src/components/pages/single/Person.jsx
@@ -1,6 +1,31 @@
+import {useEffect, useState} from "react";
+import {useParams} from "react-router-dom";
+import axios from "axios";
+
+import {CONFIG, posterImage} from "../../../config.js";
+
 function Person() {
+
+    const [person, setPerson] = useState([]);
+    const {id} = useParams();
+
+    async function fetchPerson() {
+        const {data} = await axios.get(
+            `${CONFIG.baseURL}/person/${id}?api_key=${CONFIG.apiKey}`,
+        );
+
+        setPerson(data)
+    }
+
+    useEffect(() => {
+        fetchPerson()
+    }, [id])
+
     return (
-        <div>Person</div>
+        <div>
+            <h1>{person.name}</h1>
+            <img src={posterImage(person.profile_path)}/>
+        </div>
     );
 }
 
