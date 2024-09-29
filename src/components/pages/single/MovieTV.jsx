1b79aa25af259fd52a4f98a13543efd5a8cee7d5
KhazaeiAmir110 <khazaei.amir110@gmail.com>
2024-09-29 11:49:27 +0330

    create  Add and Delete to WatchList

--- src/components/pages/single/MovieTV.jsx
+++ src/components/pages/single/MovieTV.jsx
@@ -1,13 +1,15 @@
 import {useParams} from "react-router-dom";
-import {useEffect, useState} from "react";
+import {useContext, useEffect, useState} from "react";
 import axios from "axios";
+
 import {CONFIG, posterImage} from "../../../config.js";
+import {UserContext} from "../../../context/UserContext.jsx";
 
 function MovieTV(props) {
 
     const [movieTv, setMovieTv] = useState([]);
-
     const {id} = useParams();
+    const {user, session} = useContext(UserContext);
 
     async function fetchMovieTV() {
         const {data} = await axios.get(
@@ -21,6 +23,30 @@ function MovieTV(props) {
         fetchMovieTV()
     }, [id])
 
+
+    // Add to watch MovieTV
+    function handleAddToWatchList() {
+        axios.post(
+            `${CONFIG.baseURL}/account/${user.id}/watchlist?api_key=${CONFIG.apiKey}&session_id=${session}`,
+            {
+                media_type: props.type,
+                media_id: movieTv.id,
+                watchlist: true
+            }
+        )
+    }
+
+    function handleDeleteToWatchList() {
+         axios.post(
+            `${CONFIG.baseURL}/account/${user.id}/watchlist?api_key=${CONFIG.apiKey}&session_id=${session}`,
+            {
+                media_type: props.type,
+                media_id: movieTv.id,
+                watchlist: false
+            }
+        )
+    }
+
     return (
         <div>
             <h1>{
@@ -30,7 +56,30 @@ function MovieTV(props) {
                     movieTv.name
                 )
             }</h1>
-            <img src={posterImage(movieTv.poster_path)} />
+            <img src={posterImage(movieTv.poster_path)} alt={movieTv.title}/>
+
+
+            {/*Add to Watch MovieTV*/}
+            {
+                props.type === "person" ? (
+                    ""
+                ) : (
+                    <>
+                        <button
+                            onClick={handleAddToWatchList}
+                            type="button"
+                            className="text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700">
+                            Add to watch list !
+                        </button>
+                        <button
+                            onClick={handleDeleteToWatchList}
+                            type="button"
+                            className="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700">
+                            Delete as watch list !
+                        </button>
+                    </>
+                )
+            }
         </div>
     );
 }
