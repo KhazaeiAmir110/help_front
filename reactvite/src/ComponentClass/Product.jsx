117dd7a25a1bcb0667fa39cf3ea6aae495acfee1
KhazaeiAmir110 <khazaei.amir110@gmail.com>
2024-11-18 20:27:23 +0330

    hello word by react

---
+++ reactvite/src/ComponentClass/Product.jsx
@@ -0,0 +1,12 @@
+import {Component} from "react";
+
+class Product extends Component {
+    render() {
+        return (
+            <h1>
+                Hello , Word!
+            </h1>
+        )
+    }
+}
+export default Product;
