a02f715141a06e3084942765759dabe99198e59a
KhazaeiAmir110 <khazaei.amir110@gmail.com>
2024-11-20 22:29:11 +0330

    change folder

--- reacrvite/src/App.jsx
+++ reacrvite/src/App.jsx
@@ -1,8 +1,8 @@
 import './App.css'
-import StateCount from "./assets/hooks/useState/StateCount.jsx";
-import StateInput from "./assets/hooks/useState/StateInput.jsx";
-import StateShow from "./assets/hooks/useState/StateShow.jsx";
-import EffectTest from "./assets/hooks/useEffect/useEffect.jsx";
+import StateCount from "./hooks/useState/StateCount.jsx";
+import StateInput from "./hooks/useState/StateInput.jsx";
+import StateShow from "./hooks/useState/StateShow.jsx";
+import EffectTest from "./hooks/useEffect/useEffect.jsx";
 
 export default function App() {
     return (
