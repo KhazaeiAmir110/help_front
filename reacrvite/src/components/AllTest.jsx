c0efa7971a543afec3c96a99192eacc7ed5c277b
KhazaeiAmir110 <khazaei.amir110@gmail.com>
2024-11-22 19:28:16 +0330

    create custom hook useToggle

--- reacrvite/src/components/AllTest.jsx
+++ reacrvite/src/components/AllTest.jsx
@@ -2,10 +2,14 @@ import {ContextTest, UseContextTest} from "../hooks/useContext/ContextTest.jsx";
 import {useContext} from "react";
 import ReactQuery from "./ReactQuery.jsx";
 import {FormTest} from "./FormTest.jsx";
+import {useToggle} from "../hooks/customHook/useToggle.jsx";
 
 
 export default function AllTest() {
     const {key} = useContext(ContextTest);
+
+    // custom hook
+    const [isVisible, toggle] = useToggle(true)
 
     return (
         <>
@@ -15,6 +19,11 @@ export default function AllTest() {
             <ReactQuery/>
             <hr/>
             <FormTest/>
+            <hr/>
+            <div id={"customHook"}>
+                <button onClick={toggle}>{isVisible ? "Hide" : "Show"}</button>
+                {isVisible && <h1>This is Text!!!</h1>}
+            </div>
             <hr/>
         </>
     )
