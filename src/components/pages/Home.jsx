ad023280a7233b9e48ee85bb03813b5d3616edcf
KhazaeiAmir110 <khazaei.amir110@gmail.com>
2024-11-23 10:40:31 +0330

    update active tab and pointer

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -2,11 +2,22 @@ import MovieListSlider from "../movies/MoviesListSlider.jsx";
 import {useEffect, useState} from "react";
 import axios from "axios";
 import {CONFIG} from "../../config.js";
+import * as path from "node:path";
 
 
 function Home() {
 
-    const [movies, setMovies] = useState({trending: [], onTv: [], person: []});
+    const [movies, setMovies] = useState({movie: [], onTv: [], person: []});
+
+    const [moviesActiveTab, setMoviesActiveTab] = useState("now_playing");
+
+    function handelChangeMoviesActiveTab(tab) {
+        setMoviesActiveTab(tab)
+    }
+
+    function activeClass(tab) {
+        return tab === moviesActiveTab && "text-yellow-500";
+    }
 
     async function loadApi(type, path) {
 
@@ -20,7 +31,9 @@ function Home() {
     }
 
     useEffect(() => {
-        loadApi("trending", "trending/movie/day");
+        (async () => {
+            const result = loadApi(type, path);
+        })();
     }, [])
 
     useEffect(() => {
@@ -37,16 +50,24 @@ function Home() {
             {/*Movies*/}
             <div className={"mt-12 mb-12"}>
                 <div className={"md:flex gap-16 mb-8 items-center"}>
-                    <a href={"#"}><h2 className={"text-slate-100 hover:text-yellow-500 text-4xl"}>Trending</h2></a>
+                    <a href={"#"}><h2 className={"text-slate-100 hover:text-yellow-500 text-4xl"}>Movies</h2></a>
 
-                    <ul className={"flex gap-4 uppercase text-xl"}>
-                        <li className={"hover:text-[#dcf836]"}>Today</li>
-                        <li className={"hover:text-[#dcf836]"}>This Week</li>
-                        <li className={"hover:text-[#dcf836]"}>This Month</li>
-                        <li className={"hover:text-[#dcf836]"}>This Year</li>
+                    <ul className={"flex gap-4 uppercase text-xl [&>*]:cursor-pointer"}>
+                        <li onClick={() => handelChangeMoviesActiveTab("now_playing")}
+                            className={`hover:text-[#dcf836] ${activeClass('now_playing')}`}>Now Playing
+                        </li>
+                        <li onClick={() => handelChangeMoviesActiveTab("popular")}
+                            className={`hover:text-[#dcf836] ${activeClass('popular')}`}>Popular
+                        </li>
+                        <li onClick={() => handelChangeMoviesActiveTab("top_rated")}
+                            className={`hover:text-[#dcf836] ${activeClass('top_rated')}`}>Top Rated
+                        </li>
+                        <li onClick={() => handelChangeMoviesActiveTab("upcoming")}
+                            className={`hover:text-[#dcf836] ${activeClass('upcoming')}`}>Upcoming
+                        </li>
                     </ul>
                 </div>
-                <MovieListSlider moveis={movies.trending} type="movies"/>
+                <MovieListSlider moveis={movies.movie} type="movies" activeTab={moviesActiveTab}/>
             </div>
 
             {/*TV*/}
@@ -54,8 +75,8 @@ function Home() {
                 <div className={"md:flex gap-16 mb-8 items-center"}>
                     <a href={"#"}><h2 className={"text-slate-100 hover:text-yellow-500 text-4xl"}>On TV</h2></a>
 
-                    <ul className={"flex gap-4 uppercase text-xl"}>
-                        <li className={"hover:text-[#dcf836]"}>Popular</li>
+                    <ul className={"flex gap-4 uppercase text-xl [&>*]:cursor-pointer"}>
+                        <li className={"hover:text-[#dcf836]"}>Airing Today</li>
                         <li className={"hover:text-[#dcf836]"}>Coming soon</li>
                         <li className={"hover:text-[#dcf836]"}>Top rated</li>
                     </ul>
@@ -68,7 +89,7 @@ function Home() {
                 <div className={"md:flex gap-16 mb-8 items-center"}>
                     <a href={"#"}><h2 className={"text-slate-100 hover:text-yellow-500 text-4xl"}>People</h2></a>
 
-                    <ul className={"flex gap-4 uppercase text-xl"}>
+                    <ul className={"flex gap-4 uppercase text-xl [&>*]:cursor-pointer"}>
                         <li className={"hover:text-[#dcf836]"}>Popular</li>
                         <li className={"hover:text-[#dcf836]"}>Movies</li>
                         <li className={"hover:text-[#dcf836]"}>TV shows</li>
