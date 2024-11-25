fbd2144020c12744f2098c5a17d0d9a23f35f9b2
KhazaeiAmir110 <khazaei.amir110@gmail.com>
2024-11-25 19:51:31 +0330

    add movie and person slider

---
+++ src/components/slider&card/PersonListSlider.jsx
@@ -0,0 +1,43 @@
+import {Autoplay} from "swiper/modules";
+import {Swiper, SwiperSlide} from "swiper/react";
+import MovieCard from "../movies/MovieCard.jsx";
+
+
+// eslint-disable-next-line react/prop-types
+export default function PersonListSlider({persons}) {
+    console.log(persons);
+    return (
+        <Swiper
+            modules={[Autoplay]}
+            autoplay={true} loop={true}
+            breakpoints={{
+                // when window width is >= 320px
+                640: {
+                    slidesPerView: 4,
+                    spaceBetween: 8
+                },
+                // when window width is >= 480px
+                768: {
+                    slidesPerView: 6,
+                    spaceBetween: 15
+                },
+                // when window width is >= 640px
+                1024: {
+                    slidesPerView: 9,
+                    spaceBetween: 20
+                }
+            }}>
+            {
+                persons.map(
+                    movie => (
+                        <SwiperSlide key={movie.id}>
+                            <MovieCard movie={movie} type="person"/>
+                        </SwiperSlide>
+
+                    )
+                )
+            }
+        </Swiper>
+
+    )
+}
