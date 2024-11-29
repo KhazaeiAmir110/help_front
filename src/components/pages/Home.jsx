1095db8e2b0effb9583ddf2cd6db66477445f5ee
KhazaeiAmir110 <khazaei.amir110@gmail.com>
2024-11-29 22:48:21 +0330

    Update page Home

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -30,14 +30,11 @@ function Home() {
             {/*Movies*/}
             <div className={"mt-12 mb-12"}>
                 <div className={"md:flex gap-16 mb-8 items-center"}>
-                    <a href={"#"}><h2 className={"text-slate-100 hover:text-yellow-500 text-4xl"}>Movies</h2></a>
+                    <a href={"/movies"}><h2 className={"text-slate-100 hover:text-yellow-500 text-4xl"}>Movies</h2></a>
 
-                    <ul className={"flex gap-4 uppercase text-xl [&>*]:cursor-pointer"}>
+                    <ul className={"flex gap-4 text-xl [&>*]:cursor-pointer"}>
                         <li onClick={() => handelChangeMoviesActiveTab("now_playing")}
                             className={`hover:text-[#dcf836] ${activeClassMovies('now_playing')}`}>Now Playing
-                        </li>
-                        <li onClick={() => handelChangeMoviesActiveTab("popular")}
-                            className={`hover:text-[#dcf836] ${activeClassMovies('popular')}`}>Popular
                         </li>
                         <li onClick={() => handelChangeMoviesActiveTab("top_rated")}
                             className={`hover:text-[#dcf836] ${activeClassMovies('top_rated')}`}>Top Rated
@@ -53,21 +50,16 @@ function Home() {
             {/*TV*/}
             <div className={"mt-12 mb-12"}>
                 <div className={"md:flex gap-16 mb-8 items-center"}>
-                    <a href={"#"}><h2 className={"text-slate-100 hover:text-yellow-500 text-4xl"}>On TV</h2></a>
+                    <a href={"/tv"}><h2 className={"text-slate-100 hover:text-yellow-500 text-4xl"}>On TV</h2></a>
 
-                    <ul className={"flex gap-4 uppercase text-xl [&>*]:cursor-pointer"}>
+                    <ul className={"flex gap-4 text-xl [&>*]:cursor-pointer"}>
                         <li onClick={() => handelChangeTVActiveTab("airing_today")}
                             className={`hover:text-[#dcf836] ${activeClassTV('airing_today')}`}>
                             Airing Today
                         </li>
-
-                        <li onClick={() => handelChangeTVActiveTab("popular")}
-                            className={`hover:text-[#dcf836] ${activeClassTV('popular')}`}>
-                            Popular
-                        </li>
                         <li onClick={() => handelChangeTVActiveTab("top_rated")}
                             className={`hover:text-[#dcf836] ${activeClassTV('top_rated')}`}>
-                            Top rated
+                            Top Rated
                         </li>
                     </ul>
                 </div>
@@ -77,7 +69,7 @@ function Home() {
             {/*People*/}
             <div className={"mt-12 mb-12"}>
                 <div className={"md:flex gap-16 mb-8 items-center"}>
-                    <a href={"#"}><h2 className={"text-slate-100 hover:text-yellow-500 text-4xl"}>People</h2></a>
+                    <a href={"/people"}><h2 className={"text-slate-100 hover:text-yellow-500 text-4xl"}>People</h2></a>
                 </div>
                 <FetchDataSlider type="person" activeTab="popular"/>
             </div>
