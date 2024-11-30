fafe50be76da1793ea439b21c124012fbf2678be
KhazaeiAmir110 <khazaei.amir110@gmail.com>
2024-11-30 20:52:15 +0330

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,12 @@
-import Header from "./components/header/Header.jsx";
-import Footer from "./components/footer/Footer.jsx";
 import {Outlet} from "react-router-dom";
 import {Toaster} from "react-hot-toast";
 
 function App() {
     return (
         <>
-            <Header />
             <main>
                 <Outlet/>
             </main>
-            <Footer />
             <Toaster />
         </>
     )
