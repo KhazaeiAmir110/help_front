cc283b1d4d524cbc33c0a765c4e18e9d944a720b
KhazaeiAmir110 <khazaei.amir110@gmail.com>
2024-11-30 19:35:24 +0330

    add watch list movie

--- src/components/pages/profile/Dashboard.jsx
+++ src/components/pages/profile/Dashboard.jsx
@@ -1,32 +1,55 @@
-import {useContext} from 'react';
-import {Navigate} from "react-router-dom";
+import {useEffect, useState} from 'react';
 
-import {UserContext} from "../../../context/UserContext.jsx";
-import {posterImage} from "../../../config.js";
+import {fetchData} from "../../../services/fetchData.js";
+import Card from "../../slider&card/Card.jsx";
 
 
-function Dashboard() {
-    const {session, user} = useContext(UserContext);
+function Dashboard({user}) {
+    const [watchListTv, setWatchListTv] = useState({});
+    const [watchListMovie, setWatchListMovie] = useState({});
+
+
+    async function fetchListMovie() {
+        const dataTv = await fetchData.get(`account/${user.id}/watchlist/tv`);
+        const dataMovie = await fetchData.get(`account/${user.id}/watchlist/movies`);
+        setWatchListTv(dataTv?.data)
+        setWatchListMovie(dataMovie?.data)
+    }
+
+    useEffect(() => {
+        fetchListMovie()
+    }, [])
 
     return (
-        session ? (
-            <div>
-                Dashboard
-                <div>{
-                    user ? (
-                        <div>
-                            {user.username}
-                            <img className={"rounded-full"} src={posterImage(user.avatar.tmdb.avatar_path)}
-                                 alt={"avatar"}/>
-                        </div>
-                    ) : (
-                        ""
-                    )
-                }</div>
+        <>
+            <div className="m-10">
+                <h1 className="font-medium text-2xl">Watch List Movie / Tv</h1>
+                <div className="mt-8 m-10 gap-8">
+                    <div className="flex gap-8">
+                        {
+                            watchListMovie?.results?.map(
+                                movie => (
+                                    <div className="w-48 h-96">
+                                        <Card movie={movie} type="movie" key={movie.id}/>
+                                    </div>
+                                )
+                            )
+                        }
+                    </div>
+                    <div className="flex gap-8">
+                        {
+                            watchListTv?.results?.map(
+                                movie => (
+                                    <div className="w-48 h-96">
+                                        <Card movie={movie} type="movie" key={movie.id}/>
+                                    </div>
+                                )
+                            )
+                        }
+                    </div>
+                </div>
             </div>
-        ) : (
-            <Navigate to={"/login"}/>
-        )
+        </>
     )
 
 }
