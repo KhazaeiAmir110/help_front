b25ff7b96560e400dcf4a7a96500db7110343b39
KhazaeiAmir110 <khazaei.amir110@gmail.com>
2024-12-04 22:45:43 +0330

    update config

--- src/config.js
+++ src/config.js
@@ -1,9 +1,3 @@
 export const CONFIG = {
-    apiKey: import.meta.env.VITE_API_KEY,
-    baseURL: import.meta.env.VITE_API_URL,
-    imgBaseURL: import.meta.env.VITE_IMAGE_URL,
-}
-
-export function posterImage(path, size = 'w300') {
-    return `${CONFIG.imgBaseURL}/${size}/${path}`;
+    domain: import.meta.env.REACT_APP_BASE_DOMAIN,
 }
